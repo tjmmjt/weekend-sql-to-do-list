e6ebbe0d4737620b2895e759daa0406707baa79f
Tucker Melssen <tuckerjmelssen@gmail.com>
2023-12-16 09:02:04 -0600

    Initial setup. NPM packages installed incl nodemon. router.js GET function created and tested w/ postman. Create new branch for rest of router.js

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,37 @@
-const router = require('express').Router();
-const pool = require('../modules/pool');
+// requires express and Router
+const router = require("express").Router();
+// require pool module
+const pool = require("../modules/pool");
 
+// - Server Methods ===>>> DB Interactions
+// 	- GET, POST, DELETE, PUT ===>>>
+// 	- SELECT, INSERT, DELETE, UPDATE
 
+// TODO SELECT all data from weekend-to-do-app
+// GET
+router.get("/", (req, res) => {
+  // declare queryText
+  const queryText = `SELECT * FROM "todos"`;
+  // send query to DB
+  pool
+    .query(queryText)
+    // then send results in rows
+    .then((result) => {
+      console.log(result.rows);
+      res.send(result.rows);
+    }) // catch error
+    .catch((error) => {
+      console.log(
+        "looks like you'll just have to remember what to do!:",
+        error
+      );
+    });
+});
 
+// post
+
+// put
+
+// delete
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,31 @@
+/** ---------- EXPRESS & BODY PARSER ---------- **/
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+/** ---------- ROUTE(S) ---------- **/
 const todos = require('./routes/todos.router.js');
 
+/** ---------- PORT ---------- **/
 let PORT = process.env.PORT || 5001;
 
+/** ---------- TEST ---------- **/
 // Do not modify this!
 if (process.env.NODE_ENV == 'test') {
   PORT = 5002;
 }
 
+/** ---------- MIDDLEWARE ---------- **/
+// added bodyParser.json and urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./server/public'));
 app.use(express.json());
 
+/** ---------- EXPRESS ROUTES ---------- **/
 app.use('/todos', todos);
 
+/** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
   console.log('server running on: ', PORT);
 });
